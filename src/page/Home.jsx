bb9c2e26876d43d1b2faa6e05224cbf020238152
jangrarajat <rajatjangra940@gmail.com>
2025-11-22 10:59:00 +0530

    updatin home.jsx

--- src/page/Home.jsx
+++ src/page/Home.jsx
@@ -1,72 +1,75 @@
-import React from 'react'
+import React from "react";
 
 function Home() {
+  const ladiesHighlights = [
+    "https://res.cloudinary.com/drrj8rl9n/image/upload/v1763747859/WhatsApp_Image_2025-11-21_at_18.12.08_f5f69226_bdta3o.jpg",
+    "https://res.cloudinary.com/drrj8rl9n/image/upload/v1763747860/Gemini_Generated_Image_17w7sy17w7sy17w7_kua2hd.jpg",
+    "https://res.cloudinary.com/drrj8rl9n/image/upload/v1763747861/Gemini_Generated_Image_4ejukw4ejukw4eju_lbfa1j.jpg",
+  ];
+
+  const manHighlights = [
+    "https://i.pinimg.com/1200x/2e/c7/e5/2ec7e5fbcff4b6f8cd8983d54aa8e2a1.jpg",
+    "https://i.pinimg.com/736x/38/6a/df/386adf6362f73ff6676bc0c6626d779c.jpg",
+    "https://i.pinimg.com/736x/8e/e1/b4/8ee1b48a823c94db993235cb302dab3a.jpg",
+  ];
+
   return (
-    <div className='w-full min-h-96  '>
+    <div className="w-full min-h-screen overflow-x-hidden">
+
+      {/* HERO SECTION */}
       <div
-        className="h-[90vh] bg-cover bg-center flex items-center pl-10"
-        style={{ backgroundImage: "url('https://images.pexels.com/photos/12700578/pexels-photo-12700578.jpeg')" }}
+        className="h-[80vh] bg-cover bg-center flex items-center px-6"
+        style={{
+          backgroundImage:
+            "url('https://images.pexels.com/photos/12700578/pexels-photo-12700578.jpeg')",
+        }}
       >
         <div className="text-white">
-          <h1 className="text-5xl font-bold">Style That Speaks</h1>
-          <p className="mt-4 text-xl">Discover the latest fashion</p>
-          <button className='bg-white text-black w-60 h-20 hover:scale-105 duration-300'>
+          <h1 className="text-4xl md:text-5xl font-bold">Style That Speaks</h1>
+          <p className="mt-4 text-lg md:text-xl">Discover the latest fashion</p>
+          <button className="bg-white text-black w-40 h-14 mt-4 hover:scale-105 duration-300">
             Shop Now
           </button>
         </div>
       </div>
 
-      <div id='part2' className='w-full min-h-56 relative'>
-        <div className='w-full h-20  md:px-10 flex items-center '>
-          <h2 className='text-2xl font-serif  uppercase' >[Ladies]</h2>
+      {/* LADIES SECTION */}
+      <div className="w-full mt-10">
+        <div className="w-full h-20 px-6 md:px-10 flex items-center">
+          <h2 className="text-2xl font-serif uppercase">[Ladies]</h2>
+          <p className="uppercase ml-4">highlights</p>
         </div>
 
-        <div className='w-full flex flex-col md:flex-row justify-center'>
-          <div className='w-[100%] md:w-[30%] h-[80vh] bg-cover bg-center  cursor-pointer'
-            style={{ backgroundImage: "url('https://res.cloudinary.com/drrj8rl9n/image/upload/v1763747859/WhatsApp_Image_2025-11-21_at_18.12.08_f5f69226_bdta3o.jpg')" }}
-          >
-
-          </div>
-          <div className='w-[100%] md:w-[30%] h-[80vh] bg-cover bg-center cursor-pointer'
-            style={{ backgroundImage: "url('https://res.cloudinary.com/drrj8rl9n/image/upload/v1763747860/Gemini_Generated_Image_17w7sy17w7sy17w7_kua2hd.jpg')" }}
-          >
-
-          </div>
-          <div className='w-[100%] md:w-[30%] h-[80vh] bg-cover bg-center cursor-pointer'
-            style={{ backgroundImage: "url('https://res.cloudinary.com/drrj8rl9n/image/upload/v1763747861/Gemini_Generated_Image_4ejukw4ejukw4eju_lbfa1j.jpg')" }}
-          >
-
-          </div>
+        <div className="w-full flex flex-col md:flex-row justify-center gap-4 px-4 overflow-hidden">
+          {ladiesHighlights.map((URL, i) => (
+            <div
+              key={i}
+              className="w-full md:w-1/3 h-[70vh] bg-cover bg-center cursor-pointer rounded"
+              style={{ backgroundImage: `url('${URL}')` }}
+            ></div>
+          ))}
         </div>
       </div>
 
-
-      <div id='part3' className='w-full min-h-56 relative'>
-        <div className='w-full h-20 md:px-10 flex items-center '>
-          <h2 className='text-2xl font-serif  uppercase' >[man]</h2>
+      {/* MEN SECTION */}
+      <div className="w-full mt-10">
+        <div className="w-full h-20 px-6 md:px-10 flex items-center">
+          <h2 className="text-2xl font-serif uppercase">[Man]</h2>
+          <p className="uppercase ml-4">highlights</p>
         </div>
 
-        <div className='w-full flex flex-col md:flex-row justify-center'>
-          <div className='w-[100%] md:w-[30%] h-[80vh] bg-cover bg-center  cursor-pointer'
-            style={{ backgroundImage: "url('https://i.pinimg.com/1200x/2c/56/88/2c56888db490f5ec5714102f8e9f924e.jpg')" }}
-          >
-
-          </div>
-          <div className='w-[100%] md:w-[30%] h-[80vh] bg-cover bg-center cursor-pointer'
-            style={{ backgroundImage: "url('https://i.pinimg.com/1200x/7d/07/ea/7d07ea532daa1455e71658ebaf930ba8.jpg')" }}
-          >
-
-          </div>
-          <div className='w-[100%] md:w-[30%] h-[80vh] bg-cover bg-center cursor-pointer'
-            style={{ backgroundImage: "url(' https://i.pinimg.com/1200x/a7/53/ab/a753abe7812bb46d028ec7f5eeb0960b.jpg')" }}
-          >
-
-          </div>
+        <div className="w-full flex flex-col md:flex-row justify-center gap-4 px-4 overflow-hidden">
+          {manHighlights.map((URL, i) => (
+            <div
+              key={i}
+              className="w-full md:w-1/3 h-[70vh] bg-cover bg-center cursor-pointer rounded"
+              style={{ backgroundImage: `url('${URL}')` }}
+            ></div>
+          ))}
         </div>
       </div>
-
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
